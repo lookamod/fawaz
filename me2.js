1b43d659449e06f62ea9160997c38a59b5bf2a6a
louigyz <49842535+louigyz@users.noreply.github.com>
2019-04-24 02:37:00 +0200

    Create me2.js

---
+++ me2.js
@@ -0,0 +1,294 @@
+const Discord = require('discord.js');
+const Enmap = require(`enmap`);
+const farm = new Enmap();
+const client1 = new Discord.Client();
+
+client1.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client1.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+});
+client1.login(process.env.D21);
+
+const client2 = new Discord.Client();
+client2.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client2.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client2.login(process.env.D22);
+
+const client3 = new Discord.Client();
+client3.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client3.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client3.login(process.env.D23);
+
+const client4 = new Discord.Client();
+client4.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client4.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client4.login(process.env.D24);
+
+
+const client5 = new Discord.Client();
+client5.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client5.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+});
+client5.login(process.env.D25);
+
+const client6 = new Discord.Client();
+client6.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client6.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client6.login(process.env.D26);
+
+const client7 = new Discord.Client();
+client7.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client7.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client7.login(process.env.D27);
+
+const client8 = new Discord.Client();
+client8.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client8.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+});
+client8.login(process.env.D28);
+
+const client9 = new Discord.Client();
+client9.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client9.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+});
+client9.login(process.env.D29);
+
+const client10 = new Discord.Client();
+client10.on("ready", () => {
+var interval = setInterval (function () {
+
+        client10.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client10.login(process.env.D30);
+
+const client11 = new Discord.Client();
+client11.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client11.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+});
+client11.login(process.env.D31);
+
+const client12 = new Discord.Client();
+client12.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client12.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client12.login(process.env.D32);
+
+const client13 = new Discord.Client();
+client13.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client13.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client13.login(process.env.D33);
+
+const client14 = new Discord.Client();
+client14.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client14.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+});
+client14.login(process.env.D34);
+
+const client15 = new Discord.Client();
+client15.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client15.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client15.login(process.env.D35);
+
+const client16 = new Discord.Client();
+client16.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client16.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client16.login(process.env.D36);
+
+const client17 = new Discord.Client();
+client17.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client17.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client17.login(process.env.D37);
+
+const client18 = new Discord.Client();
+client18.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client18.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client18.login(process.env.D38);
+
+const client19 = new Discord.Client();
+client19.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client19.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client19.login(process.env.D39);
+
+const client20 = new Discord.Client();
+client20.on("ready", () => {
+
+var interval = setInterval (function () {
+
+        client20.channels.get("570405467424030761").send(Math.random().toString(36).substring(7)) 
+
+}, 240000);
+
+
+
+
+});
+client20.login(process.env.D40);
