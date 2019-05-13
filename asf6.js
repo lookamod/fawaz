1330b83cede803fb5f4f8ca3fa895f34aba22dbf
louigyz <49842535+louigyz@users.noreply.github.com>
2019-05-13 15:56:29 +0200

    Update asf6.js

--- asf6.js
+++ asf6.js
@@ -1276,27 +1276,7 @@ client21.on('message', msg => {
         }
     }
 });
-client22.on('message', msg => {
-    let ownerid = '560394135115268106';
-    if(msg.author.id === ownerid){
-        if(msg.content.startsWith('c')){
 
-            if(msg.isMentioned(client22.user)){
-                farm.set(`farm`, 'blah blah')
-                let args = msg.content.split(` `)
-                msg.channel.send(`#credits <@560394135115268106> ${args[2]}`).then(()=>{
-                    const filter = m => m.author.id === msg.author.id;
-                    msg.channel.awaitMessages(filter, {max: 1, time: 29000}).then(col=>{
-                        if(col.first().content === "cancel"){
-                            return msg.reply(`Canceled!`);
-                        }
-                        msg.channel.send(`${col.first().content}`).then(()=> farm.set(`farm`, 'true'))
-                    })
-                })
-            }
-        }
-    }
-});
 
 client24.on('message', msg => {
     let ownerid = '560394135115268106';
